8664d6bebf42e9a228681e34dd0d96e96c904b6d
NelsonEAX <nelsoneax@yandex.ru>
2017-02-15 02:06:56 +0500

    попытка сборки проекта

--- src/components/Dash.vue
+++ src/components/Dash.vue
@@ -3,11 +3,11 @@
     <header class="main-header">
       <a href="/" class="logo">
         <!-- mini logo for sidebar mini 40x50 pixels -->
-        <span class="logo-mini"><img src="/static/img/logo_sm.png" alt="Logo" class="img-responsive center-block"></span>
+        <span class="logo-mini"><img src="/static/img/logo_.png" alt="Logo" class="img-responsive center-block"></span>
         <!-- logo for regular state and mobile devices -->
         <div class="container logo-lg">
-          <div class="pull-left image"><img src="/static/img/logo_sm.png" alt="Logo" class="img-responsive"></div>
-          <div class="pull-left info">CoPilot</div>
+          <div class="pull-left image"><img src="/static/img/logo_.png" alt="Logo" class="img-responsive"></div>
+          <div class="pull-left info">1000i1.ru</div>
         </div>
       </a>
 
@@ -130,45 +130,45 @@
       <section class="sidebar">
 
         <!-- Sidebar user panel (optional) -->
-        <div class="user-panel">
-          <div class="pull-left image">
+        <!--<div class="user-panel">-->
+          <!--<div class="pull-left image">-->
 
-          </div>
-          <div class="pull-left info">
-            <div><p class="white"></p></div>
-            <a href="javascript:;"><i class="fa fa-circle text-success"></i> Online</a>
-          </div>
-        </div>
+          <!--</div>-->
+          <!--<div class="pull-left info">-->
+            <!--<div><p class="white"></p></div>-->
+            <!--<a href="javascript:;"><i class="fa fa-circle text-success"></i> Online</a>-->
+          <!--</div>-->
+        <!--</div>-->
 
         <!-- search form (Optional) -->
-        <form  class="sidebar-form">
-          <div class="input-group">
-            <input type="text" name="search" id="search" class="search form-control" data-toggle="hideseek" placeholder="Search Menus" data-list=".sidebar-menu">
-                <span class="input-group-btn">
-                  <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                  </button>
-                </span>
-          </div>
-        </form>
+        <!--<form  class="sidebar-form">-->
+          <!--<div class="input-group">-->
+            <!--<input type="text" name="search" id="search" class="search form-control" data-toggle="hideseek" placeholder="Search Menus" data-list=".sidebar-menu">-->
+                <!--<span class="input-group-btn">-->
+                  <!--<button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>-->
+                  <!--</button>-->
+                <!--</span>-->
+          <!--</div>-->
+        <!--</form>-->
         <!-- /.search form -->
 
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
-          <li class="header">TOOLS</li>
-          <li class="active pageLink" ><router-link to="/"><i class="fa fa-desktop"></i><span class="page">Dashboard</span></router-link></li>
-          <li class="pageLink"><router-link to="/tables"><i class="fa fa-table"></i><span class="page">Tables</span></router-link></li>
+          <li class="header">То</li>
+          <li class="active pageLink" ><router-link to="/dash"><i class="fa fa-desktop"></i><span class="page">Меню</span></router-link></li>
+          <li class="pageLink"><router-link to="/dash"><i class="fa fa-table"></i><span class="page">Меню</span></router-link></li>
 
-          <li class="header">ME</li>
-          <li class="pageLink" ><router-link to="/tasks"><i class="fa fa-tasks"></i><span class="page">Tasks</span></router-link></li>
-          <li class="pageLink" ><router-link to="/setting"><i class="fa fa-cog"></i><span class="page">Settings</span></router-link></li>
+          <li class="header">Сё</li>
+          <li class="pageLink" ><router-link to="/dash"><i class="fa fa-tasks"></i><span class="page">Меню</span></router-link></li>
+          <li class="pageLink" ><router-link to="/dash"><i class="fa fa-cog"></i><span class="page">Меню</span></router-link></li>
 
-          <li class="header">LOGS</li>
-          <li class="pageLink"><router-link to="/access"><i class="fa fa-book"></i><span class="page">Access</span></router-link></li>
-          <li class="pageLink" ><router-link to="/server"><i class="fa fa-hdd-o"></i><span class="page">Server</span></router-link></li>
-          <li class="pageLink"><router-link to="/repos"><i class="fa fa-heart"></i><span class="page">Repos</span><small class="label pull-right bg-green">AJAX</small></router-link></li>
+          <li class="header">Пятое</li>
+          <li class="pageLink"><router-link to="/dash"><i class="fa fa-book"></i><span class="page">Меню</span></router-link></li>
+          <li class="pageLink" ><router-link to="/dash"><i class="fa fa-hdd-o"></i><span class="page">Меню</span></router-link></li>
+          <li class="pageLink"><router-link to="/dash"><i class="fa fa-heart"></i><span class="page">Меню</span><small class="label pull-right bg-green">33</small></router-link></li>
 
-          <li class="header">PAGES</li>
-          <li class="pageLink" ><router-link to="/login"><i class="fa fa-circle-o text-yellow"></i> <span class="page">Login</span></router-link></li>
+          <li class="header">Десятое</li>
+          <li class="pageLink" ><router-link to="/dash"><i class="fa fa-circle-o text-yellow"></i> <span class="page">Меню</span></router-link></li>
           <li class="pageLink" ><router-link to="/404"><i class="fa fa-circle-o text-red"></i> <span class="page">404</span></router-link></li>
         </ul>
         <!-- /.sidebar-menu -->
@@ -194,9 +194,9 @@
     <!-- /.content-wrapper -->
 
     <!-- Main Footer -->
-    <footer class="main-footer">
-      <strong>Copyright &copy;  <a href="javascript:;">CoPilot</a>.</strong> All rights reserved.
-    </footer>
+    <!--<footer class="main-footer">-->
+      <!--<strong>Copyright &copy;  <a href="javascript:;">CoPilot</a>.</strong> All rights reserved.-->
+    <!--</footer>-->
   </div>
   <!-- ./wrapper -->
 </template>
--- src/components/Page404.vue
+++ src/components/Page404.vue
@@ -4,9 +4,9 @@
         <div class="container">
           <img src="/static/img/logo.png" class="center-block logo">
           <div class="text-center col-sm-6 col-sm-offset-3">
-            <h1>You are lost.</h1>
-            <h4>This page doesn't exist.</h4>
-            <router-link to="/">Take me home.</router-link>
+            <h1>Воу воу воу</h1>
+            <h4>Эта страница не существует.</h4>
+            <router-link to="/dash">Назад.</router-link>
           </div>
         </div>
       </div>
