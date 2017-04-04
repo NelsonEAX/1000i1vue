82e7e0b49c09bd45d760c97052fcc7e81fea6266
NelsonEAX <nelsoneax@yandex.ru>
2017-04-04 07:14:53 +0500

    Добавлены категории

---
+++ src/components/my/product/Category.vue
@@ -0,0 +1,81 @@
+<template>
+    <div v-show="products.length">
+        <div class="col-md-12">
+            <div :class="{
+                'box': true,
+                'collapsed-box': !this.showCategory
+            }">
+            <!--<div class="box collapsed-box">-->
+                <div class="box-header with-border">
+                    <h3 class="box-title">{{ category.name }}</h3>
+
+                    <div class="box-tools pull-right">
+                        <button @click="switchShowCategory()" type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-box-tool dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-wrench"></i>
+                            </button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                            </ul>
+                        </div>
+                        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                    </div>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <div class="row">
+                        <product
+                                v-for="product in showProducts"
+                                :product="product"
+                        ></product>
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- ./box-body -->
+                <div class="box-footer">
+                    <div class="row">
+
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.box-footer -->
+            </div>
+            <!-- /.box -->
+        </div>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
+</template>
+
+<script>
+    import Product from './Product.vue'
+    export default {
+        data() {
+            return {
+                showCategory: false, //По умолчанию категории свернуты
+            }
+        },
+        components: {
+            'product': Product
+        },
+        computed: {
+            showProducts: function () {
+                console.log('switch');
+                return this.showCategory ? this.products : {} ;
+            }
+        },
+        methods: {
+            switchShowCategory(){
+                this.showCategory = !this.showCategory;
+            },
+        },
+        props: ['products','category'],
+    }
+</script>
+
--- src/components/my/product/Products.vue
+++ src/components/my/product/Products.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="row">
-        <product
-            v-for="product in products"
-            :product="product"
-        ></product>
+        <category
+                v-for="category in categories"
+                :category="category"
+                :products="products | thisCategoryProduct( category.id)"
+        ></category>
     </div>
 </template>
 
 <script>
     import Product from './Product.vue'
+    import Category from './Category.vue'
+
     export default {
         data () {
             return {
@@ -18,15 +21,23 @@
         },
 
         components: {
+            'category': Category,
             'product': Product
         },
-
+        filters: {
+            thisCategoryProduct: function (products, categoryID) {
+                console.log('categoryID', categoryID);
+               // console.log(products.filter( product => product.category == categoryID ))
+                return products.filter( product => product.category == categoryID );
+            }
+        },
         /*hook*/
         created (){
             console.log('Получаю категории');//###TODO: delete
             //После авторизации полуаем данные юзера с сервера
             this.$http.get('api/categories', this.$store.getters.getHeaders).then(response => {
                 console.log('категории',response.body);//###TODO: delete
+                this.categories = response.body;
                 //this.$store.dispatch('setUserInfo',response.body.user);
             })
 
@@ -34,9 +45,9 @@
             //После авторизации полуаем данные юзера с сервера
             this.$http.get('api/products', this.$store.getters.getHeaders).then(response => {
                 console.log('продукция',response.body);//###TODO: delete
-            this.products = response.body;
-            //this.$store.dispatch('setUserInfo',response.body.user);
-        })
+                this.products = response.body;
+                //this.$store.dispatch('setUserInfo',response.body.user);
+            })
         },
     }
 </script>
